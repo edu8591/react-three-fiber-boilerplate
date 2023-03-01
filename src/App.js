30467743e936f831dbc0aa54ccddd72577117c2d
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-03-01 12:22:23 -0500

    adds state and event handlers for click and hover

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import Cube from './components/Cube'
 function App() {
   return (
     <Canvas camera={{ position: [0, 0, 5] }}>
-      <Cube position={[-2, 0, 0]} name="left" wireframe />
-      <Cube position={[0, 0, 0]} name="center" wireframe={false} />
-      <Cube position={[2, 0, 0]} name="right" wireframe color={'red'} />
-      <Cube position={[0, -2, 0]} name="bottom" wireframe />
-      <Cube position={[0, 2, 0]} name="top" wireframe />
+      <Cube position={[-2, 0, 0]} name="left" wireframe args={[2, 0, 1]} />
+      <Cube
+        position={[0, 0, 0]}
+        name="center"
+        wireframe={false}
+        args={[1, 1, 1]}
+      />
+      <Cube
+        position={[2, 0, 0]}
+        name="right"
+        wireframe
+        color={'red'}
+        args={[2, 2, 1]}
+      />
+      <Cube position={[0, -2, 0]} name="bottom" wireframe args={[1, 1, 2]} />
+      <Cube position={[0, 2, 0]} name="top" wireframe args={[1, 2, 2]} />
     </Canvas>
   )
 }
--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,17 +1,34 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
 
-function Cube({ position, name, wireframe, fog, color }) {
+function Cube({ position, name, wireframe, fog, color, args }) {
+  const [hovered, setHover] = useState(false)
+  const [rotate, setRotate] = useState(false)
+
+  const handleClick = () => setRotate(!rotate)
   const ref = useRef()
   useEffect(() => {
     console.log(ref)
   })
-
+  useFrame((_state, delta) => {
+    if (rotate) {
+      ref.current.rotation.x += 1 * delta
+      ref.current.rotation.y += 0.5 * delta
+      // ref.current.position.y = Math.sin(state.clock.getElapsedTime()) //makes it move up and down
+      // ref.current.rotation.z += 2 * delta
+    }
+  })
   return (
-    <mesh position={position} name={name} ref={ref}>
-      <boxGeometry />
+    <mesh
+      position={position}
+      name={name}
+      ref={ref}
+      onPointerLeave={() => setHover(false)}
+      onPointerEnter={() => setHover(true)}
+      onClick={handleClick}>
+      <boxGeometry args={args} />
       <meshBasicMaterial
-        color={color || 0x00ff00}
+        color={hovered ? 'red' : color || 0x00ff00}
         wireframe={wireframe}
         fog={fog}
       />
